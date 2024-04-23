380b27f08681fc960d78e37c88b6f8e1f6201641
JulianKerns <j.kernstock@gmx.at>
2024-04-23 16:27:56 +0200

    fixed the issue that the connection to localhost port :8080 was refused

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
-
+	const port = "8080"
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
-
-	server := http.Server{Handler: corsMux}
-
-	server.ListenAndServe()
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
 
 }
 
