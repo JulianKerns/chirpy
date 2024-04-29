38da0a72d0d76a2e7aa16dc416807646fe092872
JulianKerns <j.kernstock@gmx.at>
2024-04-29 21:17:57 +0200

    added a path specific handler that serves static data from a fileserver

--- main.go
+++ main.go
@@ -8,11 +8,16 @@ import (
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/", fileServer)
+
 	corsMux := middlewareCors(mux)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: corsMux,
 	}
+
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
 
